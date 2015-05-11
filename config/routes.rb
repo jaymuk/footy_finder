ca10231ab4b32c109a4e04430afd2c83ed50e8e4
jaymuk <jaymuk.business@gmail.com>
2015-05-11 15:43:23 +0100

    installed Devise and added User model

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root to: "games#index"
+  devise_for :users
   resources :games
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
